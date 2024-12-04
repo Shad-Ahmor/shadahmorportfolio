2932de12024ce7f835211178abf5d37403170077
Shad-Ahmor <98160082+Shad-Ahmor@users.noreply.github.com>
2024-12-05 01:44:44 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+   assetPrefix: './',
+
   images: {
     domains: ["shadahmorportfolio.onrender.com"], // Correct domain format
     unoptimized: true, // Disable image optimization
