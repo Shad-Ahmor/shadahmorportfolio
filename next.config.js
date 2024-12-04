9ea62087abbccdc28895f0689390d6deed6f3260
Shad-Ahmor <98160082+Shad-Ahmor@users.noreply.github.com>
2024-12-05 01:22:16 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,40 +1,34 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: true,
-	images: {
-		domains: ["https://shadahmorportfolio.onrender.com/"],
-		unoptimized: true, // Disable image optimization
+  reactStrictMode: true,
+  images: {
+    domains: ["shadahmorportfolio.onrender.com"], // Correct domain format
+    unoptimized: true, // Disable image optimization
+  },
+  webpack(config) {
+    const fileLoaderRule = config.module.rules.find((rule) =>
+      rule.test?.test?.(".svg")
+    );
 
-	},
-	webpack(config) {
-		// Grab the existing rule that handles SVG imports
-		const fileLoaderRule = config.module.rules.find((rule) =>
-			rule.test?.test?.(".svg")
-		);
+    config.module.rules.push(
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/, // *.svg?url
+      },
+      {
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        resourceQuery: { not: /url/ },
+        use: ["@svgr/webpack"],
+      }
+    );
 
-		config.module.rules.push(
-			// Reapply the existing rule, but only for svg imports ending in ?url
-			{
-				...fileLoaderRule,
-				test: /\.svg$/i,
-				resourceQuery: /url/ // *.svg?url
-			},
-			// Convert all other *.svg imports to React components
-			{
-				test: /\.svg$/i,
-				issuer: /\.[jt]sx?$/,
-				resourceQuery: { not: /url/ }, // exclude if *.svg?url
-				use: ["@svgr/webpack"]
-			}
-		);
+    fileLoaderRule.exclude = /\.svg$/i;
 
-		// Modify the file loader rule to ignore *.svg, since we have it handled now.
-		fileLoaderRule.exclude = /\.svg$/i;
-
-		return config;
-	},
-	distDir: 'build', // Add this line to change the output folder to 'build'
-
+    return config;
+  },
+  // No need to change distDir for SSR
 };
 
 module.exports = nextConfig;
